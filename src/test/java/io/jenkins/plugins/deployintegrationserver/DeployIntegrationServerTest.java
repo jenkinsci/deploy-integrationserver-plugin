c57cf7f250410f628ebf786700b17e8d065cbc77
kalpshekhar <kagu@softwareag.com>
2022-11-28 15:57:27 +0000

    Update pom.xml - Restored changelog property - Deleted unused plugin dependencies. - Removed java version property.

--- src/test/java/io/jenkins/plugins/deployintegrationserver/DeployIntegrationServerTest.java
+++ src/test/java/io/jenkins/plugins/deployintegrationserver/DeployIntegrationServerTest.java
@@ -1,11 +1,8 @@
-package hudson.plugins;
+package io.jenkins.plugins.deployintegrationserver;
 
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Label;
-import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
-import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
